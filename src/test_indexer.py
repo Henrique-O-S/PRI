2c7686e6fddb4a6659b5208af19a9c501b8b8a04
Diogo Silva <82061138+DiogoSilva11@users.noreply.github.com>
2023-10-26 12:13:27 +0100

    Update test_indexer.py

--- src/test_indexer.py
+++ src/test_indexer.py
@@ -1,4 +1,7 @@
 from indexer import Indexer
 
-indexer = Indexer()
-indexer.index_all()
+solr_url = 'http://localhost:8983/solr/gettingstarted'
+solr_manager = Indexer(solr_url)
+solr_manager.clear_data()
+#solr_manager.index_articles()
+solr_manager.index_companies()
