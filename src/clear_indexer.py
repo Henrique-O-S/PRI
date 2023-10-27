cd86a8a2e2cdc4fc922eff399c6855ae7b7b5364
Diogo Filipe Ferreira da Silva <up202004288@up.pt>
2023-10-27 12:25:51 +0100

    trying to submit schema to solr

--- src/clear_indexer.py
+++ src/clear_indexer.py
@@ -3,6 +3,5 @@ from indexer import Indexer
 solr_url = 'http://localhost:8983/solr/articles'
 solr_manager = Indexer(solr_url)
 
-#solr_manager.index_articles()
-solr_manager.index_companies()
-#solr_manager.create_schema("src/schema.json")
+solr_manager.clear_data()
+solr_manager.clear_schema()
--- src/indexer.py
+++ src/indexer.py
@@ -1,10 +1,30 @@
 import pysolr
+import json
+import requests
 from Database import Database
 
 class Indexer:
     def __init__(self, solr_url):
-        self.solr = pysolr.Solr(solr_url, always_commit=True)
+        self.solr_url = solr_url
+        self.solr = pysolr.Solr(self.solr_url, always_commit=True)
         self.db = Database()
+
+    def create_schema(self, schema_file_path):
+        with open(schema_file_path, 'r') as schema_file:
+            schema_json = json.load(schema_file)
+
+        api_url = f"{self.solr_url}/schema"
+
+        headers = {'Content-type': 'application/json'}
+        with open(schema_file_path, 'r') as schema_file:
+            schema_json = schema_file.read()
+        response = requests.post(api_url, data=schema_json, headers=headers)
+
+        if response.status_code == 200:
+            print("Schema uploaded successfully.")
+        else:
+            print(f"Failed to upload the schema. Status code: {response.status_code}")
+            print(response.text)
 
     def index_articles(self):
         articles = self.db.get_all_articles()
@@ -34,5 +54,24 @@ class Indexer:
         self.solr.commit()
 
     def clear_data(self):
-        self.solr.delete(q='*:*')
-        self.solr.commit()
+        self.solr_url = self.solr_url.rstrip('/')
+        clear_url = f"{self.solr_url}/update?commit=true"
+        data = '<delete><query>*:*</query></delete>'
+        headers = {'Content-type': 'text/xml; charset=utf-8'}
+        response = requests.post(clear_url, data=data, headers=headers)
+
+        if response.status_code == 200:
+            print("Data cleared successfully.")
+        else:
+            print(f"Failed to clear data. Status code: {response.status_code}")
+            print(response.text)
+
+    def clear_schema(self):
+        api_url = f"{self.solr_url}/schema"
+        response = requests.delete(api_url)
+
+        if response.status_code == 200:
+            print("Schema cleared successfully.")
+        else:
+            print(f"Failed to clear the schema. Status code: {response.status_code}")
+            print(response.text)
