bade360485934b4ede395f4a1caf4b191df81f82
Tiago Branquinho <tiagobranquinho1@gmail.com>
2023-11-30 11:52:37 +0000

    added action when submiting input

--- src/svelte-app/src/Dropdown.svelte
+++ src/svelte-app/src/Dropdown.svelte
@@ -5,6 +5,18 @@
     function handleChange(event) {
       selectedQuery = event.target.value;
     }
+  
+    function handleKeyDown(event) {
+      if (event.key === 'Enter') {
+        // Call a function or perform an action when Enter is pressed
+        performSearch();
+      }
+    }
+  
+    function performSearch() {
+      // You can perform your search action here
+      alert(`Performing search for: ${selectedQuery}`);
+    }
   </script>
   
   <section class="section">
@@ -12,7 +24,7 @@
       <div class="field">
         <label class="label">Enter your query:</label>
         <div class="control">
-          <input class="input" type="text" bind:value={selectedQuery} on:input={handleChange} placeholder="Search...">
+          <input class="input" type="text" bind:value={selectedQuery} on:input={handleChange} on:keydown={handleKeyDown} placeholder="Search...">
         </div>
       </div>
     </div>
